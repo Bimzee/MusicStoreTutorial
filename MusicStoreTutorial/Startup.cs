4196c02f8c34868154dd8380362df6474ed4cb4b
BimalVarghese <bimal.varghese@rapidvaluesolutions.com>
2016-09-24 10:38:02 +0530

    Add project files.

---
+++ MusicStoreTutorial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MusicStoreTutorial.Startup))]
+namespace MusicStoreTutorial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
